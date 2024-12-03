9413ffe00af166ad533c7d2543599ad3c2591da7
Satyavira <viraprathama@gmail.com>
2024-12-04 00:33:02 +0700

    Modified auth_service.dart. For the sign in with email and password method change the catch to be more versatile in catching the exception.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -34,8 +34,50 @@ class Auth {
         password: password,
       );
       return userCredential.user;
-    } on FirebaseAuthException catch (e) {
-      throw Exception(e.message ?? "An error occurred while signing in.");
+    } catch (e) {
+      String errorMessage = "An error occurred. Please try again later.";
+
+      // Handle specific exception cases based on type or message
+      if (e is FirebaseAuthException) {
+        switch (e.code) {
+          case 'invalid-email':
+            errorMessage = "The email address is badly formatted.";
+            break;
+          case 'user-disabled':
+            errorMessage = "This user account has been disabled.";
+            break;
+          case 'user-not-found':
+            errorMessage = "No user found for that email address.";
+            break;
+          case 'wrong-password':
+            errorMessage = "Incorrect password provided. Please try again.";
+            break;
+          case 'invalid-credential':
+            errorMessage = "Incorrect email or password. Please check your email and password and try again.";
+            break;
+          default:
+            errorMessage = e.code;
+            break;
+        }
+      } else if (e is FormatException) {
+        errorMessage = "The email address is badly formatted.";
+      } else if (e is Exception) {
+        errorMessage = e.toString();
+      }
+      throw errorMessage;
+    }
+  }
+
+  Future<void> resendVerificationEmail() async {
+    try {
+      User? user = _firebaseAuth.currentUser;
+      if (user != null && !user.emailVerified) {
+        await user.sendEmailVerification();
+      } else {
+        throw Exception('User not logged in or email already verified.');
+      }
+    } catch (e) {
+      throw Exception('Error sending verification email: $e');
     }
   }
 
