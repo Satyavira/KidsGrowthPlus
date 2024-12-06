c88d8cb04e995602a2d1d19296b954c3c957380a
Satyavira <viraprathama@gmail.com>
2024-12-06 14:07:19 +0700

    Modified child_profile_screen.dart. Delete doctor card, list view. Change from the list view to row.

--- lib/ui/main/child_profile_screen.dart
+++ lib/ui/main/child_profile_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kids_growth_plus/styles/colors.dart';
 
 class ChildProfileScreen extends StatelessWidget {
   const ChildProfileScreen({super.key});
@@ -13,29 +16,29 @@ class ChildProfileScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const SizedBox(height: 10.0),
                 // AppBar Section
+                Center(
+                  child: Text(
+                    "Profil Anak",
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                      color: const Color(0xFF002247),
+                    ),
+                  ),
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "KidsGrowth+",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue,
-                          ),
-                        ),
-                        Text(
-                          "Profil anak",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: SvgPicture.asset(
+                        'assets/logo_color.svg',
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.contain,
+                      ),
                     ),
                     IconButton(
                       icon: const Icon(Icons.menu),
@@ -45,71 +48,58 @@ class ChildProfileScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-        
                 const SizedBox(height: 16.0),
-        
                 // Category Section
                 const Text(
                   "Kategori",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  style:
+                  TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 8.0),
-        
-                SizedBox(
-                  height: 120.0,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      _buildCategoryCard(
+                Row(
+                  children: [
+                    Expanded(
+                      child: _buildCategoryCard(
                         "Info Stunting",
                         "Fitur ini memberikan informasi seputar stunting baik itu video, blog atau artikel",
                         Colors.teal,
                         Icons.info,
                       ),
-                      _buildCategoryCard(
-                        "Peta Stunting",
-                        "Fitur ini memberikan informasi berupa peta penyebab stunting di Indonesia dan daerah",
-                        Colors.lightBlue,
-                        Icons.map,
+                    ),
+                    SizedBox(width: 16.0,),
+                    Expanded(
+                      child: _buildCategoryCard(
+                        "Konsultasi AI",
+                        "Fitur ini memberikan akses konsultasi dengan AI yang berkaitan dengan stunting",
+                        Colors.teal,
+                        Icons.android,
                       ),
-                    ],
-                  ),
-                ),
-        
-                const SizedBox(height: 16.0),
-        
-                // Medical Consultation Section
-                const Text(
-                  "Konsultasi Medis",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 8.0),
-        
-                Column(
-                  children: [
-                    _buildDoctorCard(
-                      "Dr. Samuel",
-                      "Rumah sakit Permata Bunda",
-                      "222.444",
-                      "Ahli Gizi",
-                      true,
-                    ),
-                    _buildDoctorCard(
-                      "Dr. Putri",
-                      "Rumah sakit Permata Bunda",
-                      "333.555",
-                      "Spesialis anak",
-                      false,
-                    ),
-                    _buildDoctorCard(
-                      "Dr. Wilbert",
-                      "Rumah sakit Permata Bunda",
-                      "444.666",
-                      "Ahli Gizi",
-                      false,
                     ),
                   ],
                 ),
+                SizedBox(height: 16.0,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: _buildCategoryCard(
+                        "Konsultasi Medis",
+                        "Fitur ini memberikan user akses untuk konsultasi virtual dengan tenaga medis",
+                        Colors.teal,
+                        Icons.local_hospital,
+                      ),
+                    ),
+                    SizedBox(width: 16.0,),
+                    Expanded(
+                      child: _buildCategoryCard(
+                        "Prediksi Stunting",
+                        "Fitur ini memberikan user prediksi dan keterangan tentang kondisi anak",
+                        Colors.teal,
+                        Icons.analytics,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
               ],
             ),
           ),
@@ -125,8 +115,6 @@ class ChildProfileScreen extends StatelessWidget {
       IconData icon,
       ) {
     return Container(
-      width: 200,
-      margin: const EdgeInsets.only(right: 16.0),
       decoration: BoxDecoration(
         color: color.withOpacity(0.2),
         borderRadius: BorderRadius.circular(8.0),
@@ -135,94 +123,27 @@ class ChildProfileScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(icon, color: color, size: 30),
-            const SizedBox(height: 8.0),
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: color,
+            Center(child: Icon(icon, color: color, size: 30)),
+            Center(
+              child: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: color,
+                ),
               ),
             ),
             const SizedBox(height: 8.0),
             Text(
               description,
-              style: const TextStyle(fontSize: 12, color: Colors.black54),
+              style: const TextStyle(fontSize: 12, color: darkBlue),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildDoctorCard(
-      String name,
-      String hospital,
-      String contact,
-      String specialization,
-      bool isSelected,
-      ) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: isSelected ? Colors.blue : Colors.grey,
-        ),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Row(
-        children: [
-          // Doctor Avatar
-          CircleAvatar(
-            backgroundColor: Colors.lightBlue.withOpacity(0.2),
-            child: const Icon(Icons.person, color: Colors.blue),
-          ),
-          const SizedBox(width: 16.0),
-
-          // Doctor Details
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 4.0),
-                Text(
-                  hospital,
-                  style: const TextStyle(fontSize: 14, color: Colors.black54),
-                ),
-                const SizedBox(height: 4.0),
-                Text(
-                  contact,
-                  style: const TextStyle(fontSize: 14, color: Colors.black54),
-                ),
-                const SizedBox(height: 4.0),
-                Text(
-                  specialization,
-                  style: const TextStyle(fontSize: 14, color: Colors.blue),
-                ),
-              ],
-            ),
-          ),
-
-          // Radio Button
-          Radio<bool>(
-            value: true,
-            groupValue: isSelected,
-            onChanged: (value) {
-              // Handle radio button selection
-            },
-          ),
-        ],
       ),
     );
   }
